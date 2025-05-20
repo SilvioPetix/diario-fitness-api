800a6a19b6aa2ed96789a0830de05fdc0afc6a74
Silvio Petix <silvio_91@outlook.it>
2025-05-20 18:00:56 +0200

    View modificata

--- core/views.py
+++ core/views.py
@@ -1,5 +1,4 @@
 from rest_framework import viewsets, permissions
-from django.contrib.auth.models import User
 from .models import Allenamento, DiarioAlimentare, BenessereGiornaliero, Misurazione
 from .serializers import (
     AllenamentoSerializer,
@@ -8,31 +7,24 @@ from .serializers import (
     MisurazioneSerializer,
 )
 
-class BaseUserOnlyViewSet(viewsets.ModelViewSet):
-    permission_classes = [permissions.AllowAny]
+class BasePublicViewSet(viewsets.ModelViewSet):
+    permission_classes = [permissions.AllowAny]  # Accesso aperto a tutti
 
     def get_queryset(self):
-        user = User.objects.get(username='asus')  # attenzione: alza errore se l'utente non esiste
-        return self.queryset.filter(user=user)
+        return self.queryset.all()  # Ritorna tutti gli oggetti senza filtro
 
-# Allenamento
-class AllenamentoViewSet(BaseUserOnlyViewSet):
+class AllenamentoViewSet(BasePublicViewSet):
     queryset = Allenamento.objects.all().order_by('id')
     serializer_class = AllenamentoSerializer
 
-
-
-# Diario Alimentare
-class DiarioAlimentareViewSet(BaseUserOnlyViewSet):
+class DiarioAlimentareViewSet(BasePublicViewSet):
     queryset = DiarioAlimentare.objects.all().order_by('id')
     serializer_class = DiarioAlimentareSerializer
 
-# Benessere Giornaliero
-class BenessereGiornalieroViewSet(BaseUserOnlyViewSet):
+class BenessereGiornalieroViewSet(BasePublicViewSet):
     queryset = BenessereGiornaliero.objects.all().order_by('id')
     serializer_class = BenessereGiornalieroSerializer
 
-# Misurazione
-class MisurazioneViewSet(BaseUserOnlyViewSet):
+class MisurazioneViewSet(BasePublicViewSet):
     queryset = Misurazione.objects.all().order_by('id')
     serializer_class = MisurazioneSerializer
